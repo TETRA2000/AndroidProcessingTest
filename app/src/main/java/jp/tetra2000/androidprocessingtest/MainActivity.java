90a5475f2a2464254a5bf806d6f11840785e47f6
TETRA2000 <sagittariusm25@gmail.com>
2016-03-26 17:39:20 +0900

    add empty sketch

--- app/src/main/java/jp/tetra2000/androidprocessingtest/MainActivity.java
+++ app/src/main/java/jp/tetra2000/androidprocessingtest/MainActivity.java
@@ -1,13 +1,20 @@
 package jp.tetra2000.androidprocessingtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = new Sketch();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
---
+++ app/src/main/java/jp/tetra2000/androidprocessingtest/Sketch.java
@@ -0,0 +1,17 @@
+package jp.tetra2000.androidprocessingtest;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    public void settings() {
+        size(600, 600);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+}
