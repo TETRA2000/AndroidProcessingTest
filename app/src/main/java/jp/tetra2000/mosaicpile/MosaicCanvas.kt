83b11e6015659749002236ce0fc00ab1f4741fa8
TETRA2000 <sagittariusm25@gmail.com>
2016-04-03 13:25:07 +0900

    remove async task

--- app/src/main/java/jp/tetra2000/mosaicpile/MosaicCanvas.kt
+++ app/src/main/java/jp/tetra2000/mosaicpile/MosaicCanvas.kt
@@ -10,87 +10,46 @@ class MosaicCanvas : PApplet(), MainActivity.NewImageCallback {
     private val LOG_TAG = "MosaicCanvas"
     private var lastFrame: Bitmap? = null
 
-    private var mosaicedImg: PImage? = null
-    private var mosaicTask: MosaicTask? = null
+    private var mosaicSize: Int = 10
+    set(value) {
+        if (value>=10 || value<=Math.min(width, height)) {
+            field = value
+        }
+    }
 
     override fun settings() {
-
+        size(width, height, P3D);
     }
 
     override fun setup() {
-
+        hint(DISABLE_TEXTURE_MIPMAPS);
+        noSmooth();
     }
 
     override fun draw() {
-        //        Log.d(LOG_TAG, "draw")
-        val img = mosaicedImg
-        if (img != null) {
+        if (mousePressed) {
+            mosaicSize = 50 * width / (if(mouseX > 0)  mouseX else 1)
+            Log.d("mousePressed", "mosaicSize"+mosaicSize.toString())
+        }
+
+        if (lastFrame != null) {
+            val img = PImage(lastFrame)
             img.loadPixels()
             img.resize(width, height)
             img.loadPixels()
             img.updatePixels()
-            image(img, 0f, 0f)
+            // FIXME get img width, height
+            for (y in 0..height step mosaicSize) {
+                for (x in 0..width step mosaicSize) {
+                    val color = img.get(x, y)
+                    fill(color)
+                    rect(x.toFloat(), y.toFloat(), mosaicSize.toFloat(), mosaicSize.toFloat())
+                }
+            }
         }
     }
 
     override fun onNewImage(bitmap: Bitmap) {
-        Log.d(LOG_TAG, "onNewImage: " + bitmap.width + "x" + bitmap.height)
         lastFrame = bitmap
-        createMosaicedImage()
-    }
-
-    private fun createMosaicedImage() {
-        if (lastFrame == null || mosaicTask?.status == AsyncTask.Status.RUNNING) {
-            return
-        }
-
-        mosaicTask = MosaicTask()
-        mosaicTask?.mosaicCanvas = this
-        mosaicTask?.width = width/60
-        mosaicTask?.height = height/60
-        mosaicTask?.execute(lastFrame)
-    }
-
-    private class MosaicTask: AsyncTask<Bitmap, Int, PImage>() {
-        var mosaicCanvas: MosaicCanvas? = null
-        var width = 0
-        var height = 0
-        override fun doInBackground(vararg params: Bitmap?): PImage? {
-            val bitmap = params[0]
-            if (bitmap!=null) {
-                val img = PImage(bitmap)
-                img.loadPixels()
-                img.resize(width, height)
-                img.loadPixels()
-                img.updatePixels()
-                val mosaiceSize = 40
-                for (y in 0..height step mosaiceSize) {
-                    for (x in 0..width step mosaiceSize) {
-                        val color = img.get(x, y)
-//                        Log.d("img", "color: " + color.toString())
-                        var yy = y
-                        while (yy < height) {
-                            var xx = x
-                            while (xx < width) {
-                                img.pixels[xx * yy] = color
-                                xx++
-                            }
-                            yy++
-                        }
-
-//                        Log.d("img", "(x, y): " + x.toString() + ", " + y.toString())
-                    }
-                }
-
-                img.updatePixels()
-                return img
-            }
-
-            return null
-        }
-
-        override fun onPostExecute(mosaicedImg: PImage?) {
-            mosaicCanvas?.mosaicedImg = mosaicedImg
-        }
     }
 }
